9921ced38a88c9a1665d1f8c26c53bc192960670
christiansanchez-SE <christiansanchez714@gmail.com>
2024-04-28 23:04:32 -0400

    added logging middleware

--- server.js
+++ server.js
@@ -13,6 +13,21 @@ const connectToDB = require("./config/connectToDB.js");
 //Middleware [Lets you use urlencoded in postman]
 app.use(express.urlencoded({ extended: true }));
 
+// Logging Middlewaare
+app.use((req, res, next) => {
+    const time = new Date();
+  
+    console.log(
+      `-------
+    ${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}.`
+    );
+    if (Object.keys(req.body).length > 0) {
+      console.log("Containing the data:");
+      console.log(`${JSON.stringify(req.body)}`);
+    }
+    next();
+  });
+
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 //Routes
